d4410ca37b88c93e73cf06c2dc1c34ba4e3dc69e
keenanlang <klang@aps.anl.gov>
2019-10-17 15:43:14 -0500

    Switch clones and views

--- reorg.py
+++ reorg.py
@@ -17,19 +17,9 @@ with open(sys.argv[1], "r") as input:
 	clone_sort = sorted(data.items(), key = sortOnClones, reverse=True)
 	
 	with open("stats.rst", "w") as output:
-		output.write("===================================\n")
-		output.write("Epics Modules Repository Popularity\n")
-		output.write("===================================\n")
-	
-		output.write("\n\n\n")
-		
-		output.write("Repository Views (last 14 days)\n")
-		output.write("-------------------------------\n")
-		output.write(".. csv-table::\n")
-		output.write("   :header: Module, Views\n\n")
-	
-		for item in view_sort:
-			output.write("   " + item[0].encode("ascii", "ignore") + ", " + str(item[1]["views"]) + "\n")
+		output.write("==================================\n")
+		output.write("Epics Module Repository Popularity\n")
+		output.write("==================================\n")
 		
 		output.write("\n\n\n")
 			
@@ -41,3 +31,12 @@ with open(sys.argv[1], "r") as input:
 		for item in clone_sort:
 			output.write("   " + item[0].encode("ascii", "ignore") + ", " + str(item[1]["clones"]) + "\n")
 
+		output.write("\n\n\n")
+		
+		output.write("Repository Views (last 14 days)\n")
+		output.write("-------------------------------\n")
+		output.write(".. csv-table::\n")
+		output.write("   :header: Module, Views\n\n")
+	
+		for item in view_sort:
+			output.write("   " + item[0].encode("ascii", "ignore") + ", " + str(item[1]["views"]) + "\n")
